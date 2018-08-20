cd9bf5c8760368918ee15a9583db2252137636b2
yukang <wzysz555@foxmail.com>
2018-08-20 10:31:53 +0800

    init

--- lord/src/main/java/com/yk/core/Engine.java
+++ lord/src/main/java/com/yk/core/Engine.java
@@ -1,8 +1,21 @@
 package com.yk.core;
 
+import com.yk.service.impl.Raider;
+
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * @Author: akhan
+ * @Description:
+ * @Date: 上午10:30 2018/8/20
+ */
 public class Engine {
 
+    public static Queue<String> addressQueue = new LinkedBlockingQueue<>();
+
     public static void main(String[] args) {
+        new Thread(new Raider()).run();
 
     }
 }
--- lord/src/main/java/com/yk/service/impl/Raider.java
+++ lord/src/main/java/com/yk/service/impl/Raider.java
@@ -1,11 +1,21 @@
 package com.yk.service.impl;
 
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecuteResultHandler;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.io.output.WriterOutputStream;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 /**
  * @Author: akhan
  * @Description:
  * @Date: 下午5:03 2018/8/17
  */
-public class Raider implements Runnable{
+public class Raider implements Runnable {
     /**
      * When an object implementing interface <code>Runnable</code> is used
      * to create a thread, starting the thread causes the object's
@@ -19,6 +29,19 @@ public class Raider implements Runnable{
      */
     @Override
     public void run() {
-
+        System.out.println("---------开始嗅探--------");
+        //CommandLine line = new CommandLine("nmap -v -n -Pn -p 6379 -sV --script redis-info 192.168.56.1/24");
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            Process process = runtime.exec(new String[]{"nmap", "-p", "6379", "--script", "6379", "redis-info", "192.168.0.1/24"});
+            String line = null;
+            InputStreamReader isr = new InputStreamReader(process.getInputStream());
+            BufferedReader br = new BufferedReader(isr);
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
